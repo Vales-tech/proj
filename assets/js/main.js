e33e24dde7f159bc0d7c4640a7d9e7873e6936a7
Vales-tech <145062943+Vales-tech@users.noreply.github.com>
2023-09-26 20:42:48 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,21 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://youtube.googleapis.com/youtube/v3/channels");
+xhr.send();
+xhr.responseType = "json";
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    const data = xhr.response;
+    console.log(data);
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
+
+
+
 
 (function($) {
 
