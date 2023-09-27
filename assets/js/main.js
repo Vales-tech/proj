9a5f5b2a1fd87fec0743c22cb6d88579cdd734a0
Vales-tech <145062943+Vales-tech@users.noreply.github.com>
2023-09-27 22:24:12 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,117 +4,65 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+// Gestore dell'invio del modulo
+document.getElementById("channelForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const channelName = document.getElementById("channelName").value;
 
- 
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=AIzaSyAjvgjAILjhg4tL3e713tEm2AUr2k5d9Nc");
-xhr.send();
-xhr.responseType = "json";
-xhr.onload = () => {
-  if (xhr.readyState == 4) {
-    if (xhr.status == 200) {
-      const data = xhr.response;
-      console.log(data);
-    } else {
-      console.log(`Errore nella richiesta: ${xhr.status}`);
-    }
-  }
-};
-xhr.onerror = () => {
-  console.log("Errore nella richiesta.");
-};
-const ctx = document.getElementById("myChart").getContext("2d");
-const myChart = new Chart(ctx, {
-  type: "bar", // Tipo di grafico (può essere bar, line, pie, ecc.)
-  data: {
-    labels: ["Etichetta 1", "Etichetta 2", "Etichetta 3"],
-    datasets: [
-      {
-        label: "Dati API",
-        data: [10, 20, 15], // Sostituisci con i tuoi dati API
-        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)"],
-        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)"],
-        borderWidth: 1,
-      },
-    ],
-  },
+  // Esegui una richiesta API con il nome utente del canale
+  fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&forUsername=${channelName}&key=TUA_CHIAVE_API`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Estrai le metriche desiderate dalla risposta API
+      const metrics = {
+        channelTitle: data.items[0].snippet.title,
+        subscribers: data.items[0].statistics.subscriberCount,
+        views: data.items[0].statistics.viewCount,
+        videos: data.items[0].statistics.videoCount,
+      };
+
+      // Visualizza le metriche nell'elemento div "metrics"
+      const metricsElement = document.getElementById("metrics");
+      metricsElement.innerHTML = `
+        <h2>Metriche per il canale ${metrics.channelTitle}</h2>
+        <p>Numero di iscritti: ${metrics.subscribers}</p>
+        <p>Numero di visualizzazioni: ${metrics.views}</p>
+        <p>Numero di video: ${metrics.videos}</p>
+      `;
+    })
+    .catch((error) => {
+      console.error("Si è verificato un errore durante la richiesta API:", error);
+    });
+});
+// Gestore dell'invio del modulo
+document.getElementById("channelForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const channelName = document.getElementById("channelName").value;
+
+  // Esegui una richiesta API con il nome utente del canale
+  fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&forUsername=${channelName}&key=AIzaSyAjvgjAILjhg4tL3e713tEm2AUr2k5d9Nc`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Estrai le metriche desiderate dalla risposta API
+      const metrics = {
+        channelTitle: data.items[0].snippet.title,
+        subscribers: data.items[0].statistics.subscriberCount,
+        views: data.items[0].statistics.viewCount,
+        videos: data.items[0].statistics.videoCount,
+      };
+
+      // Visualizza le metriche nell'elemento div "metrics"
+      const metricsElement = document.getElementById("metrics");
+      metricsElement.innerHTML = `
+        <h2>Metriche per il canale ${metrics.channelTitle}</h2>
+        <p>Numero di iscritti: ${metrics.subscribers}</p>
+        <p>Numero di visualizzazioni: ${metrics.views}</p>
+        <p>Numero di video: ${metrics.videos}</p>
+      `;
+    })
+    .catch((error) => {
+      console.error("Si è verificato un errore durante la richiesta API:", error);
+    });
 });
 
-
-
-
-
-
-
-
-
-
-(function($) {
-
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
-
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ '361px',   '736px'  ],
-			xsmall:  [ null,      '360px'  ]
-		});
-
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
-
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			mode: 'fade',
-			noOpenerFade: true,
-			speed: 300,
-			alignment: 'center'
-		});
-
-	// Scrolly
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height() - 5; }
-		});
-
-	// Nav.
-
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
-
-})(jQuery);
-
+ 
