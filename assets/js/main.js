5b9235350f7908104f4d96889d0d622314ca1c2d
Vales-tech <145062943+Vales-tech@users.noreply.github.com>
2023-09-27 20:47:15 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,14 +22,8 @@ $.get("GET, "https://apis.google.com/js/api.js", (data, status) => {
 
 
 
-  function authenticate() {
-    return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
-  function loadClient() {
-    gapi.client.setApiKey("YOUR_API_KEY");
+   function loadClient() {
+    gapi.client.setApiKey("AIzaSyAjvgjAILjhg4tL3e713tEm2AUr2k5d9Nc");
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(function() { console.log("GAPI client loaded for API"); },
               function(err) { console.error("Error loading GAPI client for API", err); });
