4e55f1ac745b318564efcb3fd4312c21fd6454f0
Vales-tech <145062943+Vales-tech@users.noreply.github.com>
2023-09-27 21:32:34 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,31 +20,6 @@ xhr.onload = () => {
 };
 
 
-  function loadClient() {
-    gapi.client.setApiKey("AIzaSyAjvgjAILjhg4tL3e713tEm2AUr2k5d9Nc");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
-  }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute() {
-    return gapi.client.youtube.search.list({
-      "part": [
-        "snippet"
-      ],
-      "maxResults": 25,
-      "q": "surfing"
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  };
-
-
-
-
 
 
 
