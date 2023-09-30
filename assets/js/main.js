fb89e6467df21c625613249e9c72a4e3aa24aaf5
Vales-tech <145062943+Vales-tech@users.noreply.github.com>
2023-09-30 18:25:23 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ document.getElementById("channelForm").addEventListener("submit", function (e) {
   // Esegui una richiesta API con il nome utente del canale
   fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&forUsername=${channelName}&key=AIzaSyAjvgjAILjhg4tL3e713tEm2AUr2k5d9Nc`)
     .then((response) => response.json())
-    .then((data) => {
+    ..then((data) => {
   // Estrai le metriche desiderate dalla risposta API
   const metrics = {
     channelTitle: data.items[0].snippet.title,
@@ -20,17 +20,50 @@ document.getElementById("channelForm").addEventListener("submit", function (e) {
     views: data.items[0].statistics.viewCount,
     videos: data.items[0].statistics.videoCount,
   };
-  
-  // Visualizza le metriche nell'elemento div "metrics"
-      const metricsElement = document.getElementById("metrics");
-      metricsElement.innerHTML = `
-        <h2>Metriche per il canale ${metrics.channelTitle}</h2>
-        <p>Numero di iscritti: ${metrics.subscribers}</p>
-        <p>Numero di visualizzazioni: ${metrics.views}</p>
-        <p>Numero di video: ${metrics.videos}</p>
-      `;
-    })
-    .catch((error) => {
-      console.error("Si è verificato un errore durante la richiesta API:", error);
-    });
-});
+
+  // Ottieni il contesto del canvas
+  const canvas = document.getElementById("customChart");
+  const ctx = canvas.getContext("2d");
+
+  // Imposta le dimensioni del canvas
+  canvas.width = 400;
+  canvas.height = 300;
+
+  // Definisci i dati per il grafico
+  const dataPoints = [
+    { label: "Iscritti", value: metrics.subscribers },
+    { label: "Visualizzazioni", value: metrics.views },
+    { label: "Video", value: metrics.videos },
+  ];
+
+  // Calcola la larghezza delle barre in base ai dati
+  const barWidth = 80;
+  const spacing = 40;
+  let x = 50;
+
+  // Disegna le barre del grafico
+  dataPoints.forEach((point) => {
+    const barHeight = (canvas.height - 50) * (point.value / 1000000); // Scalare per la visualizzazione
+
+    // Disegna una barra
+    ctx.fillStyle = getRandomColor();
+    ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+
+    // Scrivi l'etichetta sotto la barra
+    ctx.fillStyle = "#000";
+    ctx.fillText(point.label, x, canvas.height - 10);
+
+    // Sposta la posizione x per la prossima barra
+    x += barWidth + spacing;
+  });
+
+  // Funzione per generare colori casuali
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+})
