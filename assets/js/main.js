d4770aaed9e4d644c5d3da03209effa997719b52
Vales-tech <145062943+Vales-tech@users.noreply.github.com>
2023-10-05 23:11:32 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,34 +25,23 @@ document.getElementById("channelForm").addEventListener("submit", function (e) {
       // Crea il grafico utilizzando Chart.js
       const ctx = canvas.getContext("2d");
       const myChart = new Chart(ctx, {
-        type: "line",
-        data: {
-          labels: labels,
-          datasets: [
-            {
-              label: "Metriche del canale",
-              data: dataValues,
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)", // Colore per Subscribers
-                "rgba(54, 162, 235, 0.2)", // Colore per Views
-                "rgba(255, 206, 86, 0.2)", // Colore per Videos
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-              ],
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        },
-      });
-    });
+  labels: [
+    'Red',
+    'Green',
+    'Yellow',
+    'Grey',
+    'Blue'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: dataValues,
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(75, 192, 192)',
+      'rgb(255, 205, 86)',
+      'rgb(201, 203, 207)',
+      'rgb(54, 162, 235)'
+    ]
+  }]
+};       
 });
