811e1edc76f635b6ebc73a97b99ca496cd718ebf
Vales-tech <145062943+Vales-tech@users.noreply.github.com>
2023-10-08 21:02:20 +0200

    Update main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,36 +1,25 @@
-const express = require('express');
-const fetch = require('node-fetch');
-
-const app = express();
-const port = 3000;
-
-app.use(express.static('public')); // Serve il contenuto HTML, CSS e JavaScript dalla cartella 'public'
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html'); // Invia il file HTML principale
-});
-
-app.post('/getChannelMetrics', async (req, res) => {
-  const channelName = req.body.channelName;
+// Questo è il codice lato client che gestisce le interazioni utente nel browser
+document.getElementById("channelForm").addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const channelName = document.getElementById("channelName").value;
 
   try {
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&forUsername=${channelName}&key=AIzaSyAjvgjAILjhg4tL3e713tEm2AUr2k5d9Nc`);
-    const data = await response.json();
-    
-    const metrics = {
-      channelTitle: data.items[0].snippet.title,
-      subscribers: data.items[0].statistics.subscriberCount,
-      views: data.items[0].statistics.viewCount,
-      videos: data.items[0].statistics.videoCount,
-    };
+    const response = await fetch('/getChannelMetrics', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `channelName=${channelName}`,
+    });
 
-    res.json(metrics);
+    if (response.ok) {
+      const data = await response.json();
+      // Usa i dati per visualizzare le metriche o aggiornare il grafico
+      console.log(data);
+    } else {
+      console.error("Errore nella richiesta API");
+    }
   } catch (error) {
-    console.error("Si è verificato un errore durante la richiesta API:", error);
-    res.status(500).json({ error: "Errore durante la richiesta API" });
+    console.error("Errore durante la richiesta API:", error);
   }
-});
-
-app.listen(port, () => {
-  console.log(`Server in ascolto sulla porta ${port}`);
 });
