a88cadcebde853941f52e7f2dfae58319653803f
Vales-tech <145062943+Vales-tech@users.noreply.github.com>
2023-10-08 23:15:07 +0200

    Update main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,59 +1,45 @@
-document.getElementById("channelForm").addEventListener("submit", async function (e) {
+// Gestore dell'invio del modulo
+document.getElementById("channelForm").addEventListener("submit", function (e) {
   e.preventDefault();
   const channelName = document.getElementById("channelName").value;
 
-  try {
-    const response = await fetch('/getChannelMetrics', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',  
-      },
-      body: `channelName=${channelName}`,// Invia i dati come oggetto JSON
-    });
- 
-    if (response.ok) {
-      const data = await response.json();
-      // Usa i dati per creare il grafico
-      createPolarAreaChart(data);
-    } else {
-      console.error("Errore nella richiesta API:", response.status, response.statusText);
-    }
-  } catch (error) {
-    console.error("Errore durante la richiesta API:", error);
-  }
-});
+  // Esegui una richiesta API con il nome utente del canale
+  fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&forUsername=${channelName}&key=AIzaSyAjvgjAILjhg4tL3e713tEm2AUr2k5d9Nc`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Estrai le metriche desiderate dalla risposta API
+      const metrics = {
+        channelTitle: data.items[0].snippet.title,
+        subscribers: data.items[0].statistics.subscriberCount,
+        views: data.items[0].statistics.viewCount,
+        videos: data.items[0].statistics.videoCount,
+      };
 
-function createPolarAreaChart(data) {
-  // Seleziona l'elemento HTML in cui vuoi visualizzare il grafico
-  const ctx = document.getElementById('myChart').getContext('2d');
+      // Creazione dei dati per il grafico
+      const labels = ["Subscribers", "Views", "Videos"];
+      const dataValues = [metrics.subscribers, metrics.views, metrics.videos];
 
-  // Crea il grafico a polar area utilizzando Chart.js
-  const myChart = new Chart(ctx, {
-    type: 'polarArea',
-    data: {
-      labels: ['Subscribers', 'Views', 'Videos'],
-      datasets: [{
-        label: 'Channel Metrics',
-        data: [data.subscribers, data.views, data.videos],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.5)',
-          'rgba(54, 162, 235, 0.5)',
-          'rgba(255, 206, 86, 0.5)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-        ],
-        borderWidth: 1,
-      }],
-    },
-    options: {
-      scale: {
-        ticks: {
-          beginAtZero: true,
+      // Ottieni l'elemento canvas
+      const canvas = document.getElementById("myChart");
+
+       // Crea il grafico utilizzando Chart.js
+      const ctx = canvas.getContext("2d");
+      const myChart = new Chart(ctx, {
+        type: "polarArea", // Imposta il tipo a "polarArea"
+        data: {
+          labels: ["Subscribers", "Views", "Videos"],
+          datasets: [
+            {
+              label: "Metriche del canale",
+              data: dataValues,
+              backgroundColor: [
+                "rgb(255, 99, 132)",
+                "rgb(75, 192, 192)",
+                "rgb(255, 205, 86)",
+              ],
+            },
+          ],
         },
-      },
-    },
-  });
-}
+      });
+    });
+});
