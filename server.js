61fbbcfc103bbb36a489ed1fe88c42ba9b9d7fe6
Vales-tech <145062943+Vales-tech@users.noreply.github.com>
2023-10-08 22:42:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Abilita CORS
-app.use(cors());
-app.use(express.static('public'));
+// Middleware per analizzare i dati JSON delle richieste POST
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
+// Percorso per servire la tua pagina HTML e JavaScript
+app.use(express.static('public'));
 
+// Gestisci la richiesta POST dal client
 app.post('/getChannelMetrics', async (req, res) => {
-  const channelName = req.body.channelName;
-
   try {
-    const response = await axios.get(
-      `https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&forUsername=${channelName}&key=AIzaSyAjvgjAILjhg4tL3e713tEm2AUr2k5d9Nc`
-    );
-    const data = response.data;
+    const channelName = req.body.channelName;
+    const apiKey = 'AIzaSyAjvgjAILjhg4tL3e713tEm2AUr2k5d9Nc'; // Sostituisci con la tua chiave API YouTube
 
+    // Esegui una richiesta API con il nome utente del canale
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&forUsername=${channelName}&key=${AIzaSyAjvgjAILjhg4tL3e713tEm2AUr2k5d9Nc}`);
+    
+    // Estrai le metriche desiderate dalla risposta API
+    const data = response.data;
     const metrics = {
       channelTitle: data.items[0].snippet.title,
       subscribers: data.items[0].statistics.subscriberCount,
@@ -30,6 +29,7 @@ app.post('/getChannelMetrics', async (req, res) => {
       videos: data.items[0].statistics.videoCount,
     };
 
+    // Invia le metriche come risposta JSON
     res.json(metrics);
   } catch (error) {
     console.error("Si è verificato un errore durante la richiesta API:", error);
@@ -37,6 +37,7 @@ app.post('/getChannelMetrics', async (req, res) => {
   }
 });
 
+// Avvia il server
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
 });
