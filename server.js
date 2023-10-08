3aacde6c266da1cde32647a640dc9294365fb719
Vales-tech <145062943+Vales-tech@users.noreply.github.com>
2023-10-08 23:38:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 // Serve la cartella "public" contenente i file statici (CSS, immagini, JavaScript)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Gestisce la richiesta GET per la pagina principale
+// Gestisce la richiesta GET per la pagina left-sidebar.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'left-sidebar.html'));
 });
