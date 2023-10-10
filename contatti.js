1f12ce4319ce5c1c06e9a169fc0ec8c0f421af77
Vales-tech <145062943+Vales-tech@users.noreply.github.com>
2023-10-10 20:04:28 +0200

    Update contatti.js

--- contatti.js
+++ contatti.js
@@ -14,7 +14,7 @@ app.post('/contatti', (req, res) => {
     const contatto = req.body;
     console.log(contatto);
     contacts.push(contatto);
-    res.send('Contatto is added to the database');
+    res.send('Contatto is added to the database, Grazie per averci contattato! Abbiamo ricevuto il tuo messaggio e ti risponderemo al più presto possibile.');
 });
 
 // RITORNA LISTA CONTATTI
